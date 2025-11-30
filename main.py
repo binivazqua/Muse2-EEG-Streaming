160881a47750c29a09d04a2ae57a5344dd5c489b
binivazqua <binizavazquez@gmail.com>
2025-11-30 06:59:48 -0600

    clutch graph making and lecture taking

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ import time
 
 
 def main():
-    my_survey = CualitativeSurvey(filename="prueba_4_escalada", p_id="001")
+    my_survey = CualitativeSurvey(filename="prueba_5_estudiando_babyblue", p_id="001")
     my_survey.init_csv()
     responses = my_survey.ask_survey()
     my_survey.save_survey_response(responses)
 
     print("Survey Cualitativa Finalizada.\n")
 
-    my_eeg = EEGRecorder(duration=10, filename="prueba_4_eeg_parpadeoespaciado_escalada", r_id="001")
+    my_eeg = EEGRecorder(duration=20, filename="prueba_5_eeg_estudiandobabyblue", r_id="001")
     
     # Iniciar stream automáticamente
     if my_eeg.start_stream():
